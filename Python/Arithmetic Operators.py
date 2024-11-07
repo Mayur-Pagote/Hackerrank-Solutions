70133c4714996b3cbc496103ca5b09ce325f6f3d
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-11-07 07:02:11 +0530

    Create Arithmetic Operators.py

---
+++ Python/Arithmetic Operators.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__': 
+    a = int(input())  # Read the first integer input from the user
+    b = int(input())  # Read the second integer input from the user
+    
+    print(a + b)  # Print the sum of 'a' and 'b'
+    print(a - b)  # Print the difference when 'b' is subtracted from 'a'
+    print(a * b)  # Print the product of 'a' and 'b'
