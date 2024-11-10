6abcc5ca97f84bc5daebe548f42ed87c8ade341f
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-11-10 16:41:29 +0530

    Create Python: Division.py

---
+++ Python/Python: Division.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    
+    # Read an integer input from the user and store it in variable 'a'
+    a = int(input())
+    
+    # Read another integer input from the user and store it in variable 'b'
+    b = int(input())
+    
+    # Print the result of integer division of 'a' by 'b', which discards the decimal part
+    print(int(a / b))
+    
+    # Print the result of float division of 'a' by 'b', which includes decimals
+    print(a / b)
