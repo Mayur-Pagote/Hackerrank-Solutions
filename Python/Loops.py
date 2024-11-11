bd8bf39e00f71456a0448c2a73f13ff2669917f1
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-11-11 09:13:48 +0530

    Create Loops.py

---
+++ Python/Loops.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':  
+    n = int(input())  # Read an integer input 'n' from the user
+    
+    for i in range(n):  # Loop through integers from 0 to n-1
+        print(i * i)  # Print the square of each integer i
